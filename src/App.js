1354d1e349dcdf6078b8fd542a0abc6b6c535ec9
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2022-09-02 21:40:35 +0300

    Login comp. created.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Login from './components/Login'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Login/>
+    </div>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function Login() {
+  return <Container>
+    <img src="" alt=""/>
+    <button>Spotify bağlan</button>
+  </Container>;
+}
+
+const Container = styled.div``
